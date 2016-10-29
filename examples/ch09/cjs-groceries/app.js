cdefc45f99f3b83f026b46024c2755a55868a350
Nicolás Bevacqua <nicolasbevacqua@gmail.com>
2016-10-28 23:35:50 -0300

    backticks

--- examples/ch09/cjs-groceries/app.js
+++ examples/ch09/cjs-groceries/app.js
@@ -1,6 +1,6 @@
-const renderItem = require('./views/item');
+const renderItem = require(`./views/item`);
 const html = renderItem({
-  name: 'Banana bread',
+  name: `Banana bread`,
   amount: 3
 });
 console.log(html);
