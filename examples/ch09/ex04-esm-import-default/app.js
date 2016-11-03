5099fed2a835839e5b293e21bffb7188eae65289
Nicolás Bevacqua <nicolasbevacqua@gmail.com>
2016-11-02 21:13:20 -0300

    more examples, module syntax

---
+++ examples/ch09/ex04-esm-import-default/app.js
@@ -0,0 +1,4 @@
+import counter from './counter';
+
+console.log(counter);
+// <- 0
---
+++ examples/ch09/ex05-esm-import-named/counter.js
@@ -0,0 +1,4 @@
+let counter = 0;
+const increment = () => counter++;
+const decrement = () => counter--;
+export { counter as default, increment, decrement }
